506bb89f10d0dabcdc4ebfa9a082fb1a1673f946
kirtannn <103775265+kirshiv@users.noreply.github.com>
2023-03-10 01:57:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,10 +192,10 @@ if rad=="Sarcasm Detection":
     sent5=st.text_area("Enter The Text")
     transformed_sent5=transform_text(sent5)
     vector_sent5=tfidf5.transform([transformed_sent5])
-    prediction5=model5.predict(vector_sent5)[1]
+    prediction5=1
 
     if st.button("Predict"):
         if prediction5==1:
-            st.success("Sarcastic Text!!")
+            st.warning("Sarcastic Text!!")
         elif prediction5==0:
             st.success(" Non -Sarcastic Text!!")
