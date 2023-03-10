a0c33b9db7a0f5d59aecea2c9180984aa5d97c7a
kirtann RAINA <103775265+kirshiv@users.noreply.github.com>
2023-03-10 12:26:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,21 +13,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 
 nltk.download('punkt')
 nltk.download('stopwords')
 sw=nltk.corpus.stopwords.words("english")
 
-rad=st.sidebar.radio("Navigation",["Home","Sarcasm Detection"])
+rad=st.sidebar.radio("Navigation",["Home","Spam or Ham Detection","Sentiment Analysis","Stress Detection","Hate and Offensive Content Detection","Sarcasm Detection"])
 
 #Home Page
 if rad=="Home":
-    st.title("Sarcasm Detection")
+    st.title("Complete Text Analysis App")
+    st.image("Complete Text Analysis Home Page.jpg")
     st.text(" ")
-   
+    st.text("The Following Text Analysis Options Are Available->")
+    st.text(" ")
+    st.text("1. Spam or Ham Detection")
+    st.text("2. Sentiment Analysis")
+    st.text("3. Stress Detection")
+    st.text("4. Hate and Offensive Content Detection")
+    st.text("5. Sarcasm Detection")
 
 #function to clean and transform the user input which is in raw format
 def transform_text(text):
@@ -186,12 +190,9 @@ if rad=="Sarcasm Detection":
     transformed_sent5=transform_text(sent5)
     vector_sent5=tfidf5.transform([transformed_sent5])
     prediction5=model5.predict(vector_sent5)[0]
-  
-
-   
 
     if st.button("Predict"):
         if prediction5==1:
             st.exception("Sarcastic Text!!")
         elif prediction5==0:
-            st.success(" Non -Sarcastic Text!!")
+            st.success("Non Sarcastic Text!!"
